bd5b9cd7a2f975e7c9b200f03e071adb456dd1f3
YaolinTan <yaolint@nycstudents.net>
2025-01-30 12:20:44 -0500

    e

--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/main.css'
+
+createApp(App).mount('#app')
 import './assets/main.css'
 
 import { createApp } from 'vue'

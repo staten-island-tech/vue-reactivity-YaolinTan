b202edf675500b2595b9d56050fab737d239ef38
YaolinTan <yaolint@nycstudents.net>
2025-02-10 12:31:49 -0500

    e

--- app/src/components/PickPlayer.vue
+++ app/src/components/PickPlayer.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
     <div class="box">
-      <form action="submit" @submit.prevent="">
+      <form @submit.prevent="navigateToAbout">
         <p>Select an option:</p>
-        <label> <input type="radio" name="choice" value="option1" /> Option 1 </label><br />
-        <label> <input type="radio" name="choice" value="option2" /> Option 2 </label><br />
-        <label> <input type="radio" name="choice" value="option3" /> Option 3 </label><br />
+        <label>
+          <input type="radio" name="choice" :value="player1.name" v-model="selectedPlayer" />
+          {{ player1.name }} </label
+        ><br />
+        <label>
+          <input type="radio" name="choice" :value="player2.name" v-model="selectedPlayer" />
+          {{ player2.name }} </label
+        ><br />
+        <label>
+          <input type="radio" name="choice" :value="player3.name" v-model="selectedPlayer" />
+          {{ player3.name }} </label
+        ><br />
         <button class="submit-button" type="submit">Submit</button>
       </form>
+      <h2>Selected Player: {{ selectedPlayer }}</h2>
     </div>
   </div>
 </template>
@@ -15,16 +25,27 @@
 <script setup>
 import { players } from './array'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router' // Import useRouter for navigation
 
-const randomNumber1 = Math.floor(Math.random() * 99)
-const randomNumber2 = Math.floor(Math.random() * 99)
-const randomNumber3 = Math.floor(Math.random() * 99)
+const router = useRouter() // Create the router instance
 
-let player1 = players[randomNumber1]
-let player2 = players[randomNumber2]
-let player3 = players[randomNumber3]
+const randomNumber1 = Math.floor(Math.random() * players.length)
+const randomNumber2 = Math.floor(Math.random() * players.length)
+const randomNumber3 = Math.floor(Math.random() * players.length)
 
-console.log(player1, player2, player3)
+const player1 = players[randomNumber1]
+const player2 = players[randomNumber2]
+const player3 = players[randomNumber3]
+
+const selectedPlayer = ref('') // Initialize selectedPlayer
+
+console.log(player1)
+console.log(player2)
+console.log(player3)
+
+const navigateToAbout = () => {
+  router.push({ name: 'about' })
+}
 </script>
 
 <style scoped>
